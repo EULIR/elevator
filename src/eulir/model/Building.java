ded95d174b9cf0fa246f96f50468ada3423dc331
EULIR <runmingli_edu@163.com>
2018-11-04 22:13:25 +0800

    update

--- src/eulir/model/Building.java
+++ src/eulir/model/Building.java
@@ -3,13 +3,14 @@ package eulir.model;
 import java.util.Arrays;
 
 public class Building {
-	Elevator[] build;
-	Floor[] floors;
+	private Elevator[] build;
+	private Floor[] floors;
 
 	public Building(int numOfElevators, int numOfFloors) {
 		build = new Elevator[numOfElevators];
 		floors = new Floor[numOfFloors];
 		Arrays.setAll(floors, i -> new Floor(i + 1));
-
+		Arrays.setAll(build, Elevator::new);
 	}
+
 }
--- src/eulir/model/Floor.java
+++ src/eulir/model/Floor.java
@@ -6,9 +6,21 @@ public class Floor {
 
 	public Floor(int floor) {
 		number = floor;
+		controller = new Controller(this.number);
 	}
 }
 
 class Controller {
+	private int request;
+	private int floor;
 
+	public Controller(int floor) {
+		this.floor = floor;
+	}
+
+	//TODO: I haven't thought about the solution or structure clearly yet
+	public void setRequest(int requestFloor) {
+		this.request = requestFloor;
+
+	}
 }
