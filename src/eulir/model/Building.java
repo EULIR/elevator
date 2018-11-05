82cd9b5ee616d91623ced290035596f024c19a8f
EULIR <runmingli_edu@163.com>
2018-11-05 23:33:53 +0800

    recode

--- src/eulir/model/Building.java
+++ src/eulir/model/Building.java
@@ -1,16 +1,26 @@
 package eulir.model;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Building {
 	private Elevator[] build;
-	private Floor[] floors;
+	private int floors;
+	private Stack<Integer> request;
 
 	public Building(int numOfElevators, int numOfFloors) {
 		build = new Elevator[numOfElevators];
-		floors = new Floor[numOfFloors];
-		Arrays.setAll(floors, i -> new Floor(i + 1));
+		floors = numOfFloors;
 		Arrays.setAll(build, Elevator::new);
+		request = new Stack<>();
 	}
+
+	public void outsideRequest(int floorNum) {
+		if (floorNum <= floors && floorNum >= 1)
+			request.push(floorNum);
+		else
+			throw new IllegalArgumentException("floor not available");
+	}
+
 
 }
