553999396e9227ffd51e68d47dd0bbaabfab1784
EULIR <runmingli_edu@163.com>
2018-11-06 21:15:57 +0800

    add TODO

--- src/eulir/model/Algorithm.java
+++ src/eulir/model/Algorithm.java
@@ -1,6 +1,7 @@
 package eulir.model;
 
 public class Algorithm {
+	//TODO: different algorithms to implement best elevator choices
 	public static int chooseElevator(int[] elevatorStatus, int targetFloor) {
 		return 0;
 	}
